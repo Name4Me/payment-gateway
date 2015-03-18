b2e679e2ed5b02d2601f5926bf3cae4f0e485fae
Name4Me <7380003@gmail.com>
2015-03-18 15:28:01 +0200

    Update

--- src/engine.php
+++ src/engine.php
@@ -19,6 +19,30 @@
 
 <?php
 
+    if (!isset($_POST) || !isset($_POST['customerFullName'])
+        || !isset($_POST['creditCardHolderName']) || !isset( $_POST['creditCardNumber'])
+        || !isset($_POST['creditCardExpiration']) || !isset($_POST['amount']) || !isset($_POST['currency'])
+        || !isset($_POST['creditCardCCV'])) return 'error: don`t found POST data';
+
+    $result = paymentGateway(
+        $_POST['customerFullName'],
+        $_POST['creditCardHolderName'],
+        $_POST['creditCardNumber'],
+        $_POST['creditCardExpiration'],
+        $_POST['amount'],
+        $_POST['currency'],
+        $_POST['creditCardCCV']
+    );
+
+    echo '      '.$result;
+?>
+        <br>
+    </body>
+</html>
+
+<?php
+
+function paymentGateway($customerFullName, $creditCardHolderName, $creditCardNumber, $creditCardExpiration, $amount, $currency, $creditCardCCV){
     require_once dirname(__DIR__) . '/vendor/autoload.php';
     require_once __DIR__ .'/BraintreePayment.php';
     require_once __DIR__ .'/PayPalPayment.php';
@@ -26,8 +50,6 @@
 
     try {
         $ini_array = parse_ini_file(dirname(__DIR__) . "/config/config.ini",true);
-        $message = $ini_array['errorMessages']['emessage0'];
-        if (!isset($_POST)) return;
         $configBranch = 'braintreeConfiguration';
 
         $payment = new BraintreePayment(
@@ -37,18 +59,18 @@
             $ini_array[$configBranch]['privateKey']
         );
 
-        $payment->setCustomer($_POST['customerFullName']);
+        $payment->setCustomer($customerFullName);
 
         if ($payment->is_Customer()) $payment->setCreditCard(
-            $_POST['creditCardHolderName'],
-            $_POST['creditCardNumber'],
-            $_POST['creditCardExpiration'],
-            $_POST['creditCardCCV']
+            $creditCardHolderName,
+            $creditCardNumber,
+            $creditCardExpiration,
+            $creditCardCCV
         );
 
         if ($payment->is_CreditCard()) $cardType = $payment->getCreditCard()->cardType;
-            elseif ($ini_array[$configBranch]['environment'] == 'sandbox')
-                $cardType = cardTypeByNumber($_POST['creditCardNumber']);
+        elseif ($ini_array[$configBranch]['environment'] == 'sandbox')
+            $cardType = cardTypeByNumber($creditCardNumber);
 
         if (!empty($cardType)) {
             switch ($cardType) {
@@ -56,25 +78,23 @@
                 case 'American Express':
                     $paymentMethod = 'Paypal';
                     //if currency is not USD and credit card is AMEX, return error message, that AMEX is possible to use only for USD
-                    if ($_POST['currency'] != 'USD') {
-                        $message = $ini_array['errorMessages']['emessage1'];
-                        return;
+                    if ($currency != 'USD') {
+                        return $ini_array['errorMessages']['emessage1'];
                     }
                     break;
                 default:
                     //if currency is USD, EUR, or AUD, then use Paypal. Otherwise use Braintree.
-                    if (is_numeric(strpos('USD,EUR,AUD',$_POST['currency']))) $paymentMethod = 'Paypal';
+                    if (is_numeric(strpos('USD,EUR,AUD',$currency))) $paymentMethod = 'Paypal';
                     else $paymentMethod = 'Braintree';
                     break;
             }
         } else {
-            $message = 'Error Unknown Card Type.';
-            return;
+            return $ini_array['errorMessages']['emessage0'];
         }
 
         switch ($paymentMethod) {
             case "Braintree":
-                $message = $payment->doPayment($_POST['price'], $ini_array[$configBranch][$_POST['currency']]);
+                return $payment->doPayment($amount, $ini_array[$configBranch][$currency]);
                 break;
 
             case "Paypal":
@@ -86,28 +106,20 @@
                 );
 
                 $paypalPayment->setCreditCard(
-                    $_POST['creditCardHolderName'],
-                    $_POST['creditCardNumber'],
-                    $_POST['creditCardExpiration'],
-                    $_POST['creditCardCCV'],
+                    $creditCardHolderName,
+                    $creditCardNumber,
+                    $creditCardExpiration,
+                    $creditCardCCV,
                     $cardType
                 );
 
-                $message = $paypalPayment->doPayment($_POST['price'],$_POST['currency']);
+                return $paypalPayment->doPayment($amount,$currency);
                 break;
         }
 
 
     } catch (Exception $e) {
-        $message .= '<p>error: '.'\n'.$e->getMessage().'</p>';
+        return '<p>error: '.'\n'.$e->getMessage().'</p>';
     }
-
-
-    echo '      '.$message;
-?>
-        <br>
-    </body>
-</html>
-
-<?php
+}
 
