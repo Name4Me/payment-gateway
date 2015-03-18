e8112517ea795c9aa014a5dd60b249f8a32f2974
Name4Me <7380003@gmail.com>
2015-03-18 15:27:22 +0200

    Create

---
+++ tests/EngineTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trp
+ * Date: 18.03.15
+ * Time: 15:04
+ */
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) .'/src/engine.php';
+
+/**
+ * Class EngineTest
+ */
+class EngineTest extends PHPUnit_Framework_TestCase {
+
+    public function testFunctions() {
+        $result = paymentGateway(
+            'Name Surname',
+            'Name Surname',
+            '4417119669820331',
+            '12/20',
+            '10',
+            'USD',
+            '874'
+        );
+        $this->assertStringStartsWith('<h3>Payment success!</h3>',$result);
+        $result = paymentGateway(
+            'Name Surname',
+            'Name Surname',
+            '4111111111111111',
+            '12/20',
+            '10',
+            'THK',
+            '111'
+        );
+        $this->assertStringStartsWith($result,'<p>error:');
+        $result = paymentGateway(
+            'Name Surname',
+            'Name Surname',
+            '4111111111111111',
+            '12/20',
+            '10',
+            'SGD',
+            '111'
+        );
+        $this->assertStringStartsWith('<h3>Payment success!</h3>',$result);
+    }
+
+}
