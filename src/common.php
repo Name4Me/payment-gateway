a5792db98d8591b17db9cced83b5ed2b6a61e0c4
Name4Me <7380003@gmail.com>
2015-03-18 13:41:36 +0200

    Update

--- src/common.php
+++ src/common.php
@@ -6,6 +6,23 @@
  * Time: 16:16
  */
 
+// Car//CreditCard Type
+const AMEX = 'American Express';
+const CARTE_BLANCHE = 'Carte Blanche';
+const CHINA_UNION_PAY = 'China UnionPay';
+const DINERS_CLUB_INTERNATIONAL = 'Diners Club';
+const DISCOVER = 'Discover';//+
+const JCB = 'JCB';
+const LASER = 'Laser';
+const MAESTRO = 'Maestro';
+const MASTER_CARD = 'MasterCard';
+const SOLO = 'Solo';
+const SWITCH_TYPE = 'Switch';
+const VISA = 'Visa';
+const VISA_ELECTRON = 'Visa Electron';
+const UATP = 'UATP';
+const UNKNOWN = 'Unknown';
+
 /**
  * @param $fullName
  * @param $paymentID
@@ -29,3 +46,93 @@ function save2DB ($fullName,$paymentID, $details) {
         $dbh->exec($sql);
         $dbh = null;
 }
+
+/**
+ * @param $number
+ * @return string
+ */
+function cardTypeByNumber($number){
+    $type = UNKNOWN;
+
+    if (strlen($number)>0) {
+        $firstDigit = (int)$number[0];
+        if (strlen($number)>3) $first4Digit = (int) substr($number,0,4);
+        if ($firstDigit == 1) {
+            $type = UATP;
+            return $type;
+        }
+        if ($firstDigit == 4) {
+            $type = VISA;
+            if ((!empty($first4Digit)) && (in_array($first4Digit,[4026, 417500, 4405, 4508, 4844, 4913, 4917]))) $type = VISA_ELECTRON;
+            if ((!empty($first4Digit)) && (in_array($first4Digit,[4903, 4905, 4911, 4936]))) $type = SWITCH_TYPE;
+            return $type;
+        }
+    }
+
+
+    if (strlen($number)>1) {
+        $first2Digit = (int) substr($number,0,2);
+
+
+        switch ($first2Digit) {
+
+            case 30:
+                $type = CARTE_BLANCHE;
+                break;
+
+            case 34:
+            case 37:
+                $type = AMEX;
+                break;
+
+            case 36:
+            case 38:
+            case 39:
+                $type = DINERS_CLUB_INTERNATIONAL;
+                break;
+
+            case 35:
+                $type = JCB;
+                break;
+
+            case 50:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 61:
+            case 63:
+            case 66:
+            case 67:
+            case 68:
+            case 69:
+                $type = MAESTRO;
+                if ((!empty($first4Digit)) && (in_array($first4Digit,[6334, 6767]))) $type = SOLO;
+                if ((!empty($first4Digit)) && (in_array($first4Digit,[6304, 6706, 6771, 6709]))) $type = LASER;
+                if ((!empty($first4Digit)) && (in_array($first4Digit,[5641, 6331, 6333, 6759]))) $type = SWITCH_TYPE;
+                break;
+
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+                $type = MASTER_CARD;
+                break;
+
+            case 60:
+            case 62:
+            case 64:
+            case 65:
+                $type = DISCOVER;
+                break;
+            case 88:
+                $type = CHINA_UNION_PAY;
+                break;
+
+
+        }
+    }
+        return $type;
+}
+
